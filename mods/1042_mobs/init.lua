a92777b826e4b6bb5bb867158ce4e88d30f11b3e
Xeno333 <Xeno333@mailfence.com>
2024-11-05 23:01:52 -0600

    update more stuff

---
+++ mods/1042_mobs/init.lua
@@ -0,0 +1,10 @@
+
+
+core.register_entity("1042_mobs:fish", {
+    initial_properties = {
+        visual = "mesh",
+        mesh = "fish.glb",
+        textures = {"1042_plain_node.png^[colorize:#ff4400:128"},
+        hp_max = 1,
+    },
+})
