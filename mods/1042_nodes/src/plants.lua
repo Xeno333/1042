2819f4d7eaaf217a58cc55c037edaade8ad861b8
Futuray <123812211+Futuray-pgm@users.noreply.github.com>
2025-02-28 18:42:21 +0100

    sapling basic node (fixed) (#15)
    * sapling basic node (fixed)
    
    * re-add flowers

--- mods/1042_nodes/src/plants.lua
+++ mods/1042_nodes/src/plants.lua
@@ -90,27 +90,24 @@ core.register_node("1042_nodes:apple", {
     groups = {plant = 1, food = 1, breakable_by_hand = 1},
 })
 
-
-
--- WIP
-core.register_node("1042_nodes:sapling_plain", {
-    description = "Plain Sapling",
+core.register_node("1042_nodes:sapling", {
+    description = "Sapling",
     drawtype = "mesh",
-    mesh = "fruit.obj", -- Filler for now
-    wield_scale = {x = 0.1, y = 0.1, z = 0.1},
+    mesh = "sapling.obj",
     tiles = {
-		"1042_plain_node.png^[colorize:#672307:200",
-		"1042_plain_node.png^[colorize:#278b13:168"
+		"1042_plain_node.png^[colorize:#278b13:168",
+		"1042_plain_node.png^[colorize:#672307:200"
 	},
-    use_texture_alpha = "blend",
+    use_texture_alpha = "opaque",
 
     paramtype = "light",
     sunlight_propagates = true,
     walkable = true,
     buildable_to = false,
 
-    groups = {plant = 1, breakable_by_hand = 1, not_in_creative_inventory = 1},
+    groups = {plant = 1, breakable_by_hand = 1},
 })
+
 
 -- Flowers
 
