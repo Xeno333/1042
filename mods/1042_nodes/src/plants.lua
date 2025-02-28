a64343e3aa84480c2ac6bc6a240504fa1df2a59c
Futuray <123812211+Futuray-pgm@users.noreply.github.com>
2025-02-28 18:36:38 +0100

    sapling basic node (fixed)

--- mods/1042_nodes/src/plants.lua
+++ mods/1042_nodes/src/plants.lua
@@ -90,105 +90,20 @@ core.register_node("1042_nodes:apple", {
     groups = {plant = 1, food = 1, breakable_by_hand = 1},
 })
 
-
-
--- WIP
-core.register_node("1042_nodes:sapling_plain", {
-    description = "Plain Sapling",
+core.register_node("1042_nodes:sapling", {
+    description = "Sapling",
     drawtype = "mesh",
-    mesh = "fruit.obj", -- Filler for now
-    wield_scale = {x = 0.1, y = 0.1, z = 0.1},
+    mesh = "sapling.obj",
     tiles = {
-		"1042_plain_node.png^[colorize:#672307:200",
-		"1042_plain_node.png^[colorize:#278b13:168"
+		"1042_plain_node.png^[colorize:#278b13:168",
+		"1042_plain_node.png^[colorize:#672307:200"
 	},
-    use_texture_alpha = "blend",
+    use_texture_alpha = "opaque",
 
     paramtype = "light",
     sunlight_propagates = true,
     walkable = true,
     buildable_to = false,
 
-    groups = {plant = 1, breakable_by_hand = 1, not_in_creative_inventory = 1},
-})
-
--- Flowers
-
-core.register_node("1042_nodes:flower_1", {
-    description = "Flower",
-    drawtype = "mesh",
-    mesh = "flower_1.obj",
-    tiles = {
-        "1042_plain_node.png^[colorize:#11AA22:64", -- stem
-        "1042_plain_node.png^[colorize:#AA2211:64", -- petal
-        "1042_plain_node.png^[colorize:#22AA11:64"  -- leaf
-    },
-    use_texture_alpha = "opaque",
-
-    paramtype = "light",
-    sunlight_propagates = true,
-    floodable = true,
-    walkable = false,
-    buildable_to = false,
-
-    groups = {plant = 1, attached_node = 3, breakable_by_hand = 1, burns = 1, flower = 1},
-})
-
-core.register_node("1042_nodes:flower_2", {
-    description = "Flower",
-    drawtype = "mesh",
-    mesh = "flower_2.obj",
-    tiles = {
-        "1042_plain_node.png^[colorize:#119922:64", -- stem
-        "1042_plain_node.png^[colorize:#AA1177:64", -- petal
-    },
-    use_texture_alpha = "opaque",
-
-    paramtype = "light",
-    sunlight_propagates = true,
-    floodable = true,
-    walkable = false,
-    buildable_to = false,
-
-    groups = {plant = 1, attached_node = 3, breakable_by_hand = 1, burns = 1, flower = 2},
-})
-
-core.register_node("1042_nodes:flower_3", {
-    description = "Flower",
-    drawtype = "mesh",
-    mesh = "flower_3.obj",
-    tiles = {
-        "1042_plain_node.png^[colorize:#11AA22:64", -- stem
-        "1042_plain_node.png^[colorize:#7711AA:64", -- petal
-        "1042_plain_node.png^[colorize:#22AA11:64"  -- leaf
-    },
-    use_texture_alpha = "opaque",
-
-    paramtype = "light",
-    sunlight_propagates = true,
-    floodable = true,
-    walkable = false,
-    buildable_to = false,
-
-    groups = {plant = 1, attached_node = 3, breakable_by_hand = 1, burns = 1, flower = 3},
-})
-
-core.register_node("1042_nodes:flower_4", {
-    description = "Flower",
-    drawtype = "mesh",
-    mesh = "flower_4.obj",
-    tiles = {
-        "1042_plain_node.png^[colorize:#11AA22:64", -- stem
-        "1042_plain_node.png^[colorize:#FFAA11:64", -- petal
-        "1042_plain_node.png^[colorize:#22AA11:64"  -- leaf
-    },
-    use_texture_alpha = "opaque",
-
-    paramtype = "light",
-    sunlight_propagates = true,
-    floodable = true,
-    walkable = false,
-    buildable_to = false,
-
-    groups = {plant = 1, attached_node = 3, breakable_by_hand = 1, burns = 1, flower = 4},
+    groups = {plant = 1, breakable_by_hand = 1},
 })
