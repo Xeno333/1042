969767936deca006e516bc51a6c1a37d60ed0388
Python-Sargent <samuel@thesargents.org>
2025-07-18 18:38:29 -0400

    Update init.lua
    Add node registry support, change rarity coloring. Begin commentation in registry modspace.

--- mods/1042_core/code/1042_registry/init.lua
+++ mods/1042_core/code/1042_registry/init.lua
@@ -2,26 +2,25 @@ registry_1042 = {
     items = {}
 }
 
-registry_1042.rarity = {
-    [0] = {name="Common", color="#aaa"},
-    [1] = {name="Unusual", color="#3b3"},
-    [2] = {name="Rare", color="33b"},
-    [3] = {name="Singular", color="b33"},
-    [4] = {name="Legendary", color="993"},
-    [5] = {name="Unique", color="939"},
+registry_1042.rarity = { -- rarity definition
+    [1] = {name="Common", color="#aaa"},
+    [2] = {name="Unusual", color="#3b3"},
+    [3] = {name="Rare", color="#39b"},
+    [4] = {name="Unique", color="#b33"},
+    [5] = {name="Legendary", color="#cb3"},
+    [6] = {name="Singular", color="#939"},
 }
 
--- {"1042_core:iron_ingot 2", "1042_core:sticks}
-
 local function recipe_rarity(recipe)
-    if recipe == nil then
-        return 0
+    if recipe == nil then -- if there is no recipe default to base rarity
+        return 1
     end
-    r = 0
+
+    r = 1
     c = 0
     for _, v in ipairs(recipe) do
         local count = 1
-        s = v.split(" ")
+        s = v.split(" ") -- split the `item count` string into the respective pieces
         local ingredient = s[1]
 
         if s[2] ~= nil then
@@ -30,86 +29,103 @@ local function recipe_rarity(recipe)
 
         local it = registry_1042.items[ingredient] or nil
         if it ~= nil and it.rarity ~= nil then
-            r = r + tonumber(it.rarity) * count
+            r = r + tonumber(it.rarity) * count -- multiply by the defined rarity
+        else
+            r = r + 1 * count -- multiply by base rarity if none defined
         end
         c = c + count
     end
-    local rarity = math.floor(r / c + 0.5)
+    local rarity = math.floor(r / c + 0.5) -- round the remainder of the division off to make it an integer (+0.5 to round up)
     return rarity
 end
 
-function registry_1042.register_craftitem(name, def, rarity, recipe)
-    nname = "1042_core:" .. name
+function registry_1042.register_item(name, def, rarity, recipe)
+    nname = "1042_core:" .. name -- names follow modname:itemname convention
 
     local ndef = {}
     for k, v in pairs(def) do
-        ndef[k] = v
+        ndef[k] = v -- add in all the definition key/value pairs
     end
 
     local r = nil
-    if rarity ~= nil then
+    if rarity ~= nil then -- if rarity defined, assign it
         r = rarity
     else
-        r = recipe_rarity(recipe)
+        r = recipe_rarity(recipe) -- otherwise try to get the rarity from the recipe
     end
 
     if recipe ~= nil then
-        core_1042.crafting.register_craft({
+        core_1042.crafting.register_craft({ -- if recipe defined then register it
             result = nname,
             type = "1042_default",
             items = recipe
         })
     end
 
-    registry_1042.items[nname] = {
+    registry_1042.items[nname] = { -- add the item to the list so we can get rarity easily (could use the nodedef._rarity?)
         name = nname,
         rarity = r
     }
 
-    ndef._rarity = r or 0
+    ndef._rarity = r or 1
 
     rcolor = registry_1042.rarity[r].color
     rlabel = registry_1042.rarity[r].name
 
     tx = ndef.inventory_image or ndef.textures[1]
-    ndef.inventory_image = tx .. "^(rarity.png^[colorize:" .. rcolor .. ":255)"
+    ndef.inventory_image = tx .. "^(rarity.png^[colorize:" .. rcolor .. ":255)" -- TODO: Handle inventory images for nodes and models better
     core.log(ndef.inventory_image)
     ndef.description = ndef.description .. "\n" .. core.colorize(rcolor, rlabel)
 
-    core.register_craftitem(nname, ndef)
+    if def.item_type == "craftitem" then -- register it as a craftitem or node respectively
+        core.register_craftitem(nname, ndef)
+    elseif def.item_type == "node" then
+        core.register_node(nname, ndef)
+    end
 end
 
-registry_1042.register_craftitem("test_0", {
+
+--[[
+registry_1042.register_item("test_0", {
     description = "Test thingy 0",
-    textures = {"test_stick.png"}
-}, 0)
+    textures = {"test_stick.png"},
+    item_type = "craftitem",
+}, 1)
 
-registry_1042.register_craftitem("test_1", {
+registry_1042.register_item("test_1", {
     description = "Test thingy 1",
-    textures = {"test_stick.png"}
-}, 0)
+    textures = {"test_stick.png"},
+    item_type = "craftitem",
+}, 2)
 
-registry_1042.register_craftitem("test_2", {
+registry_1042.register_item("test_2", {
     description = "Test thingy 2",
-    textures = {"test_stick.png"}
-}, 0)
+    textures = {"test_stick.png"},
+    item_type = "craftitem",
+}, 3)
 
-registry_1042.register_craftitem("test_3", {
+registry_1042.register_item("test_3", {
     description = "Test thingy 3",
-    textures = {"test_stick.png"}
-}, 0)
+    textures = {"test_stick.png"},
+    item_type = "craftitem",
+}, 4)
 
-registry_1042.register_craftitem("test_4", {
+registry_1042.register_item("test_4", {
     description = "Test thingy 4",
-    textures = {"test_stick.png"}
-}, 0)
+    textures = {"test_stick.png"},
+    item_type = "craftitem",
+}, 5)
 
-registry_1042.register_craftitem("test_5", {
+registry_1042.register_item("test_5", {
     description = "Test thingy 5",
-    textures = {"test_stick.png"}
-}, 0)
+    textures = {"test_stick.png"},
+    item_type = "craftitem",
+}, 6)
 
-registry_1042.register_craftitem("test_r", {
+registry_1042.register_item("test_r", {
     description = "Test thingy R",
-    textures = {"test_stick.png"}
+    textures = {"test_stick.png"},
+    item_type = "craftitem",
 }, nil, {"1042_core:sticks 10"})
+
+]]--
